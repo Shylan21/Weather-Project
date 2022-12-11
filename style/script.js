3cf9dc6f03f1312de2532dcac3e9df61f35e0f08
Shylan21 <isabel.tartarelli@live.it>
2022-12-11 18:10:00 +0000

    Added API icon

--- style/script.js
+++ style/script.js
@@ -60,6 +60,13 @@ function weatherCondition(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document.querySelector("alt", response.data.weather[0].description);
 }
 
 function currentLocation(event) {
